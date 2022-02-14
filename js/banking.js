d30cb97d9d9221d9da313151e58ed0da9c4448de
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-02-14 12:24:34 +0600

    deposite completed

--- js/banking.js
+++ js/banking.js
@@ -2,9 +2,26 @@
     document.getElementById("deposite-button").addEventListener('click',function(){
         //get the amount deposited............
     const inputListener = document.getElementById("deposite-amount");
-    const depositeAmount = inputListener.value;
-    console.log(depositeAmount);
+    const depositeAmountText= inputListener.value;
+    const depositeAmount = parseFloat(depositeAmountText);
+
 
     const depositTotal=document.getElementById('deposit-total');
-     depositTotal.innerText = depositeAmount;
-    })
+    const correntDepositeAmountText = depositTotal.innerText;
+    const correntDepositeAmount = parseFloat(correntDepositeAmountText)
+
+    const newDepostTotal=correntDepositeAmount +depositeAmount;
+     depositTotal.innerText = newDepostTotal;
+
+     //update Account Balance...............
+    const BalanceTotal = document.getElementById('Balance-total');
+    const BalanceTotalText = BalanceTotal.innerText;
+    const previousBalanceTotal=parseInt(BalanceTotalText);
+    const newBalanceTotal= previousBalanceTotal+newDepostTotal;
+    BalanceTotal.innerText=newBalanceTotal;
+
+     //clear the deposite input field;
+     depositeAmount.value = '';
+    });
+
+    
